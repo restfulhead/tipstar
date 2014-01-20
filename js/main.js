8faabea3b9b6efa50cc124d8f77123678f115152
Patrick Ruhkopf <patrickruhkopf@gmail.com>
2014-01-19 21:00:13 -0500

    design and usability improvements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// pop up for formular tooltip
 $(function ()
 { $(".popover-link").popover();
 });
+
+// hide navigation on click
+$(function() {
+    $('.nav a').on('click', function(){
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
+});
+
